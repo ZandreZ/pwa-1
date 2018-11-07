c6caa34ee4dd2cadfda3a273072bea3147f3c680
Feijao Costa <feijao@feijaocosta.com.br>
2018-11-07 19:14:17 -0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,7 @@ if (typeof files == 'undefined') {
   files.push('./');
 }
 
-var CACHE_NAME = 'shopping-v13';
+var CACHE_NAME = 'feijao-v1';
 
 self.addEventListener('activate', function(event) {
   console.log('[SW] Activate');
